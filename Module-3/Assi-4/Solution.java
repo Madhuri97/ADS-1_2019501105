d4b65157e82d17c9892612b676bfcc0a60899efd
madhuri <madhiurinaidu1997@msitprogram.net>
2019-11-02 10:52:17 +0530

    by madhuri

--- Module-3/Assi-4/Solution.java
+++ Module-3/Assi-4/Solution.java
@@ -1,9 +1,24 @@
 class Solution{
+
 	public Team[] sort(Team[] teams){
 		// your code goes here
+		//selection sort 
+		int n = teams.length;
+		for(int i = 0; i < n - 1; i++){
+			int min = i;
+			for(int j = i + 1; j < n; j++){
+				if(teams[j].compareTo(teams[min]) == 1){
+					min = j;
+				}
+			}
+			Team temp = teams[i];
+			teams[i] = teams[min];
+			teams[min] = temp;
+			}
 		return teams;
 	}
 }
+
 class Team implements Comparable<Team> {
 	String teamName;
 	int noOfWins;
@@ -15,8 +30,37 @@ class Team implements Comparable<Team> {
 		noOfWins = wins;
 		noOfLosses = losses;
 	}
+
 	public String toString(){
 		//retrun all the attributes as a string but appending with ", "
-        return "";
+		return "";
+	}
+	public int compareTo(Team that){
+		//primary conditions for number of wins
+        if(this.noOfWins > that.noOfWins) {  
+            return 1;
+        }
+        else if(this.noOfWins < that.noOfWins) {	
+            return -1;
+		}
+		//primary conditions for losses
+        else{
+            if(this.noOfLosses > that.noOfLosses) { 	 
+                return -1;
+            }
+            else if(this.noOfLosses < that.noOfLosses) {
+                return 1;
+			}
+			//primary conditions for draw matches
+            else{
+            if(this.noOfDraws > that.noOfDraws) {
+                return 1;
+            }
+            else if(this.noOfDraws < that.noOfDraws) {
+                return -1;
+            }
+          }
+        }
+        return 0;
     }
 }

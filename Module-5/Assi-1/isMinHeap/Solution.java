d227098fabf7feda7de6874d14fad83ee3f9cf38
madhuri <madhiurinaidu1997@msitprogram.net>
2019-11-05 14:11:16 +0530

    by madhuri

--- Module-5/Assi-1/isMinHeap/Solution.java
+++ Module-5/Assi-1/isMinHeap/Solution.java
@@ -1,4 +1,8 @@
-class Solution{
+/**
+ * @author Madhuri
+ * //min heap is bottom up approach
+ */
+class Solution {
 	public static boolean isMinHeap(double[] arr) {
 		int n = arr.length;
 		if(n <= 0) {
@@ -6,6 +10,7 @@ class Solution{
 		}
 		for(int i = n - 1; i >= 0; i--) {
 			if(n > i/2) {
+				//comparing parent and child and if needed exchange it with parent 
 				if(arr[i/2] > arr[i]) {
 					return false;
 				}
